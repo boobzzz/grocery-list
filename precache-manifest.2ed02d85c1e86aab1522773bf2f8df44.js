7e72f228056b6a76e8fcd7c628a6acf3ae6026b3
Yuriy Borys <iam618384@gmail.com>
2020-10-03 21:08:42 +0300

    Updates

---
+++ precache-manifest.2ed02d85c1e86aab1522773bf2f8df44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c250d2cd302e36d6dda330e0bc3a4d",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "55a997d7b74ada5c0b33",
+    "url": "/grocery-list/static/css/main.e5af1ea5.chunk.css"
+  },
+  {
+    "revision": "0716c3aada42b3735296",
+    "url": "/grocery-list/static/js/2.6306168f.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/grocery-list/static/js/2.6306168f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a997d7b74ada5c0b33",
+    "url": "/grocery-list/static/js/main.1cf17a5e.chunk.js"
+  },
+  {
+    "revision": "3f79d6f9011e342088c4",
+    "url": "/grocery-list/static/js/runtime-main.a83510f9.js"
+  }
+]);
